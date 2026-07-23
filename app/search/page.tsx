b5a3d2deff38f63044749e4c486c5e4ef068614d
shahramRezvani <53124952+shahramRezvani@users.noreply.github.com>
2026-07-23 17:07:45 +0330

    Add app/search/page.tsx

---
+++ app/search/page.tsx
@@ -0,0 +1,10 @@
+import SearchBar from '@/components/SearchBar';
+
+export default function SearchPage() {
+  return (
+    <div className="p-6">
+      <h1 className="mb-6 text-3xl font-bold">جستجو</h1>
+      <SearchBar />
+    </div>
+  );
+}
